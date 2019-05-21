4b340c6e0db83b74d9bd3bcff95dd1f08242ea9c
HuangShao1314 <1052912975@qq.com>
2019-05-21 21:09:29 +0800

    页面目录更改

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const resolve = dir => path.join(__dirname, dir)
 module.exports = {
-  publicPath: '/',
+  publicPath: '/scroll-side-menu/',
   outputDir: 'dist',
   assetsDir: 'assets',
   lintOnSave: true,
